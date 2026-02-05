574b78392edca896c3ae239445519ace59b6c398
Jared Gonzalez <94082555+JamesIGT@users.noreply.github.com>
2026-02-04 19:40:09 -0500

    Final Changes

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -45,7 +45,7 @@ export default function WelcomeSection() {
       </div>
       <div className="photo-container">
         <img id="photo" src="/img/photo3.png" alt="Foto de perfil" width="200" />
-        <a href="/cv.pdf" download className="download-cv-btn">
+        <a href="../public/doc/CV_Jared_Gonzalez.pdf" download className="download-cv-btn">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
             <polyline points="7 10 12 15 17 10"></polyline>
